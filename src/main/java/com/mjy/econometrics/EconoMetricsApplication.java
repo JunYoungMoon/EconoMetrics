9f446d17f1330f39820edc5c43c9d4810871cf64
문준영 <ko5642027@naver.com>
2023-03-16 23:53:15 +0900

    first commit

---
+++ src/main/java/com/mjy/econometrics/EconoMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.mjy.econometrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconoMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EconoMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mjy/econometrics/EconoMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjy.econometrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconoMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
