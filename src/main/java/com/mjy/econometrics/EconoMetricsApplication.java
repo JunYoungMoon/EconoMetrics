25aa610b85387c74b89f803fe62c3def6bee8a7f
문준영 <ko5642027@naver.com>
2023-03-18 22:32:07 +0900

    DB연결 완료

--- src/main/java/com/mjy/econometrics/EconoMetricsApplication.java
+++ src/main/java/com/mjy/econometrics/EconoMetricsApplication.java
@@ -2,8 +2,10 @@ package com.mjy.econometrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
+@EnableJdbcHttpSession
 public class EconoMetricsApplication {
 
     public static void main(String[] args) {
