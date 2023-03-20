84e0974c9af4aa20f7c83c6a4484d92333671336
문준영 <ko5642027@naver.com>
2023-03-20 23:45:18 +0900

    post 방식 변경

--- src/main/java/com/mjy/econometrics/controller/CpiDataController.java
+++ src/main/java/com/mjy/econometrics/controller/CpiDataController.java
@@ -3,7 +3,7 @@ package com.mjy.econometrics.controller;
 import com.mjy.econometrics.model.CpiData;
 import com.mjy.econometrics.repository.CpiDataRepository;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,21 +20,13 @@ public class CpiDataController {
         this.cpiDataRepository = cpiDataRepository;
     }
 
-
-    @GetMapping
+    @PostMapping
     public List<CpiData> getCpiData(@RequestParam(value = "start_date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
                                     @RequestParam(value = "end_date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
         if (startDate != null && endDate != null) {
-            return getCpiDataByDateRange(startDate, endDate);
+            return cpiDataRepository.findByDateBetweenOrderByDateAsc(startDate, endDate);
         } else {
             return cpiDataRepository.findAllByOrderByDateAsc();
         }
     }
-
-    @GetMapping
-    public List<CpiData> getCpiDataByDateRange(@RequestParam("start_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
-                                               @RequestParam("end_date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate) {
-        return cpiDataRepository.findByDateBetweenOrderByDateAsc(startDate, endDate);
-    }
-
 }
