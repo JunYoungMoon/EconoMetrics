388ff82086bbb248be5e90f74703efc55de23ee5
문준영 <ko5642027@naver.com>
2023-03-21 23:32:04 +0900

    setter, getter

--- src/main/java/com/mjy/econometrics/model/CpiData.java
+++ src/main/java/com/mjy/econometrics/model/CpiData.java
@@ -1,6 +1,8 @@
 package com.mjy.econometrics.model;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "cpi_data")
@@ -10,10 +12,49 @@ public class CpiData {
     private Long id;
 
     @Column(name = "date")
-    private java.sql.Date date;
+    private LocalDate date;
 
     @Column(name = "cpi_value")
-    private Double cpiValue;
+    private BigDecimal cpiValue;
 
-    // Getter, Setter, 생성자
+    public CpiData() {
+    }
+
+    public CpiData(LocalDate date, BigDecimal cpiValue) {
+        this.date = date;
+        this.cpiValue = cpiValue;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public BigDecimal getCpiValue() {
+        return cpiValue;
+    }
+
+    public void setCpiValue(BigDecimal cpiValue) {
+        this.cpiValue = cpiValue;
+    }
+
+    @Override
+    public String toString() {
+        return "CpiData{" +
+                "id=" + id +
+                ", date=" + date +
+                ", cpiValue=" + cpiValue +
+                '}';
+    }
 }
