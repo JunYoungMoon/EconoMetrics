14c398e28522a7d97fb68124a2ea3ed5b298c613
문준영 <ko5642027@naver.com>
2023-04-16 22:48:31 +0900

    sso 연동작업

--- src/main/java/com/mjy/econometrics/controller/ChartController.java
+++ src/main/java/com/mjy/econometrics/controller/ChartController.java
@@ -1,24 +1,14 @@
 package com.mjy.econometrics.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mjy.econometrics.model.CpiModel;
-import com.mjy.econometrics.model.PceModel;
-import com.mjy.econometrics.repository.CpiRepository;
-import com.mjy.econometrics.repository.PceRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/chart-data")
