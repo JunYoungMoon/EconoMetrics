767af730f10f5f53fa227f81a02e0594a180f0bc
문준영 <ko5642027@naver.com>
2023-04-23 22:11:43 +0900

    sso 연동작업

--- src/main/java/com/mjy/econometrics/config/SecurityConfig.java
+++ src/main/java/com/mjy/econometrics/config/SecurityConfig.java
@@ -1,13 +1,39 @@
 package com.mjy.econometrics.config;
 
-
+import com.mjy.econometrics.service.CustomOAuth2UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/", "/home", "/login**", "/error**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(customOAuth2UserService))
+                );
+        return http.build();
+    }
 }
+
 
 
---
+++ src/main/java/com/mjy/econometrics/service/CustomOAuth2UserService.java
@@ -0,0 +1,34 @@
+package com.mjy.econometrics.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        // 원하는 사용자 정보를 가져오거나 변환하는 코드를 여기에 추가
+
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        return new DefaultOAuth2User(
+                User.withUsername(oAuth2User.getAttribute(userNameAttributeName))
+                        .password("")
+                        .roles("USER")
+                        .build()
+                        .getAuthorities(),
+                oAuth2User.getAttributes(),
+                userNameAttributeName
+        );
+    }
+}
+
+
