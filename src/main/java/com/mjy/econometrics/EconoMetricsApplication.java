f0975e08e85563a6775beb6eefd5640127249a13
문준영 <ko5642027@naver.com>
2023-04-30 23:45:31 +0900

    구글 sso 추가 완료

--- src/main/java/com/mjy/econometrics/EconoMetricsApplication.java
+++ src/main/java/com/mjy/econometrics/EconoMetricsApplication.java
@@ -2,10 +2,8 @@ package com.mjy.econometrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
-@EnableJdbcHttpSession
 public class EconoMetricsApplication {
 
     public static void main(String[] args) {
