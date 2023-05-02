52c41ebb5fa24f8faeebb2ce75b8e6247bcaa5e5
문준영 <ko5642027@naver.com>
2023-05-02 23:17:03 +0900

    security config 설정중

--- src/main/java/com/mjy/econometrics/config/SecurityConfig.java
+++ src/main/java/com/mjy/econometrics/config/SecurityConfig.java
@@ -3,30 +3,36 @@ package com.mjy.econometrics.config;
 
 import com.mjy.econometrics.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig implements WebSecurityConfigurer<WebSecurity> {
+public class SecurityConfig {
 
     @Autowired
     private CustomOAuth2UserService customOAuth2UserService;
 
-    @Override
-    public void init(WebSecurity web) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/css/**", "/js/**", "/images/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService)
+                .and()
+                .defaultSuccessUrl("/loginSuccess")
+                .failureUrl("/loginFailure");
 
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-
-
+        return http.build();
     }
 }
-
 
 
 
