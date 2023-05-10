78a7d1ca635f48d12ecd77581298206fdd8c76aa
문준영 <ko5642027@naver.com>
2023-05-10 23:51:46 +0900

    sso 로그아웃 설정중

--- src/main/java/com/mjy/econometrics/config/SecurityConfig.java
+++ src/main/java/com/mjy/econometrics/config/SecurityConfig.java
@@ -1,19 +1,11 @@
 package com.mjy.econometrics.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -28,40 +20,9 @@ public class SecurityConfig {
                 .and()
                 .oauth2Login()
                 .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessHandler(oidcLogoutSuccessHandler())
-                .permitAll()
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+                .logout().logoutUrl("/custom-logout");  // custom logout url
 
         return http.build();
-    }
-
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
-                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository());
-
-        // 로그아웃 후 리다이렉트할 URL 설정
-        oidcLogoutSuccessHandler.setDefaultTargetUrl("http://localhost:8085/");
-
-        return oidcLogoutSuccessHandler;
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
-    }
-
-    @Autowired
-    private Environment env;
-
-    private ClientRegistration googleClientRegistration() {
-        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
-                .clientId(env.getProperty("spring.security.oauth2.client.registration.google.client-id"))
-                .clientSecret(env.getProperty("spring.security.oauth2.client.registration.google.client-secret"))
-                .scope("openid", "email", "profile")
-                .build();
     }
 }
 
--- src/main/java/com/mjy/econometrics/controller/UserController.java
+++ src/main/java/com/mjy/econometrics/controller/UserController.java
@@ -5,9 +5,15 @@ import com.mjy.econometrics.config.JwtUtil;
 import com.mjy.econometrics.dto.AuthResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class UserController {
@@ -32,8 +38,23 @@ public class UserController {
     }
 
     // 로그아웃 엔드포인트 추가
-    @GetMapping("/logout")
-    public String logout() {
-        return "로그아웃";
+    @GetMapping("/custom-logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        if (authentication != null && authentication.isAuthenticated()) {
+            OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+            String provider = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+
+            if ("google".equals(provider)) {
+                // 구글 세션 종료
+                String logoutUrl = "https://accounts.google.com/logout?continue=http://localhost:8085"; // 로그아웃 후 리다이렉트할 URL
+                String idToken = (String) oauth2User.getAttribute("id_token");
+                String redirectUrl = logoutUrl + "&id_token_hint=" + idToken;
+                response.sendRedirect(redirectUrl);
+                return;
+            }
+        }
+
+        // 기본 로그아웃 처리
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
     }
 }
